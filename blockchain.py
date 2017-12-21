2f4706cf92e11eb39a9ee77cc29ab1aa7e028e4c
Silvano Cerza <silvanocerza@gmail.com>
2017-12-21 20:31:32 +0100

    implemented rough proof of work algorithm

--- blockchain.py
+++ blockchain.py
@@ -66,3 +66,34 @@ class Blockchain:
     @property
     def last_block(self):
         return self.chain[-1]
+
+    def proof_of_work(self, last_proof):
+        """
+        Simple Proof of Work Algorithm:
+         - Find a number p' such that hash(pp') contains 4 leading zeroes, where
+            p is the previous p'
+         - p is the previous proof, and p' is the new proof
+
+         :param last_proof: <int>
+         :return: <int>
+        """
+
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        """
+        Validates proof: does hash(last_proof, proof) contains 4 leading zeroes?
+
+        :param last_proof: <int> Previous Proof
+        :param proof: <int> Current Proof
+        :return: <bool> True if correct, False if not
+        """
+
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
